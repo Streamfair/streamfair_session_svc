5bf55e66b5f3fe73da0a062a8910e6e1085d676f
Fabian Quist <frog_from_the_lake@gmx.de>
2024-02-13 14:44:44 +0100

    added http2 to grpc gateway server

--- gapi/server.go
+++ gapi/server.go
@@ -11,6 +11,8 @@ import (
 	"github.com/Streamfair/streamfair_session_svc/pb"
 	"github.com/Streamfair/streamfair_session_svc/util"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
@@ -21,6 +23,7 @@ import (
 // Server serves gRPC requests for the streamfair user management service.
 type Server struct {
 	grpcServer *grpc.Server
+	httpServer *http.Server
 	pb.UnimplementedSessionServiceServer
 	config    util.Config
 	store     db.Store
@@ -33,6 +36,7 @@ func NewServer(config util.Config, store db.Store) (*Server, error) {
 		config:     config,
 		store:      store,
 		grpcServer: grpc.NewServer(),
+		httpServer: &http.Server{Handler: nil},
 		healthSrv:  health.NewServer(),
 	}
 
@@ -53,7 +57,7 @@ func (server *Server) RunGrpcServer() error {
 	if err != nil {
 		// Update the health status to NOT_SERVING if there's an error.
 		server.healthSrv.SetServingStatus("", grpc_health_v1.HealthCheckResponse_NOT_SERVING)
-		return fmt.Errorf("server: error while creating listener: %v", err)
+		return fmt.Errorf("server: error while creating gRPC listener: %v", err)
 	}
 
 	log.Printf("start gRPC server on %s", listener.Addr().String())
@@ -66,7 +70,7 @@ func (server *Server) RunGrpcServer() error {
 	return nil
 }
 
-// RunGrpcGatewayServer runs a gRPC gateway server on the given address.
+// RunGrpcGatewayServer runs a gRPC gateway server that translates HTTP requests into gRPC calls.
 func (server *Server) RunGrpcGatewayServer() error {
 	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
 		MarshalOptions: protojson.MarshalOptions{
@@ -89,11 +93,17 @@ func (server *Server) RunGrpcGatewayServer() error {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
+	// Wrap the mux with h2c.NewHandler to support both HTTP/1 and HTTP/2 connections.
+	handler := h2c.NewHandler(mux, &http2.Server{})
+
+	// Set the httpServer handler to the wrapped mux.
+	server.httpServer.Handler = handler
+
 	// Add a route for the health check service.
 	mux.HandleFunc("/v1/healthz", func(w http.ResponseWriter, r *http.Request) {
 		resp, err := server.healthSrv.Check(context.Background(), &grpc_health_v1.HealthCheckRequest{})
 		if err != nil {
-			// Log the error and respond with an internal server error status code.
+			// Log the error for debugging purposes
 			log.Printf("Health check failed: %v", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte("Internal Server Error"))
@@ -112,11 +122,11 @@ func (server *Server) RunGrpcGatewayServer() error {
 
 	listener, err := net.Listen("tcp", server.config.HttpServerAddress)
 	if err != nil {
-		return fmt.Errorf("server: error while creating listener: %v", err)
+		return fmt.Errorf("server: error while creating HTTP listener: %v", err)
 	}
 
 	log.Printf("start HTTP Gateway server on %s", listener.Addr().String())
-	err = http.Serve(listener, mux)
+	err = server.httpServer.Serve(listener)
 	if err != nil {
 		return fmt.Errorf("server: error while starting HTTP Gateway server: %v", err)
 	}
